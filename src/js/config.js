1ac054c4e71467f944e4ea5f8325b2df363ad71a
quzhichao <quzhichao@lunaon.com>
2019-03-06 13:38:38 +0800

    remove panel and add config

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export default {
+  viewBoxWidth: 500,
+  viewBoxHeight: 500 / (screen.availWidth / screen.availHeight),
+  paddingLeft: 10,
+  paddingBottom: 50
+}
--- src/js/game.js
+++ src/js/game.js
@@ -1,5 +1,6 @@
 import Polygon from './polygon'
 import Util from './util'
+import config from './config'
 
 /**
  * 游戏类
@@ -13,8 +14,6 @@ class Game {
     // 获取相应元素
     this.picture = document.querySelector('#picture')
     this.picturePolygon = document.querySelectorAll('#picture polygon')
-    this.polygonItem = document.querySelectorAll('.polygon-item')
-    this.polygonItemSvg = document.querySelectorAll('.polygon-item svg')
 
     this.pictureStartPoint = {
       0: [{ x: 20.3, y: 561.1 }, { x: 127.6, y: 648.6 }, { x: 109.3, y: 612.7 }],
@@ -27,7 +26,8 @@ class Game {
     this.isDraggable = false // 是否在拖动中
     this.currDraggableNum = 0 // 当前拖拽的第几个
     this.textIndexDomArr = []  // 序号dom集合
-    
+    this.polygonItem = [] // 等待着被拖动的polygon和text
+
     this.initObject()
     this.event()
   }
@@ -83,27 +83,15 @@ class Game {
       new Polygon(this.picturePolygon[59]),
       new Polygon(this.picturePolygon[72])
     ]
-    this.initPolygonWrap()
     this.initPicture()
   }
 
-  initPolygonWrap () {
-    this.polygonArr.forEach((item, index) => {
-      const animatedPoints = item.polygonDom.animatedPoints
-      let str = ''
-      for (let i = 0; i < animatedPoints.length; i++) {
-        const elem = animatedPoints[i]
-        // 所有点减去外接矩形的x与y
-        str += `${elem.x - item.x},${elem.y - item.y} `
-      }
-      // 将碎片填入panel的左上角
-      this.polygonItemSvg[index].innerHTML = `<polygon style="fill:${item.color};" points="${str} "/>
-                                              <text x="${item.center.x - item.x - 5}" y="${item.center.y - item.y + 3}">${index + 1}</text>`
-      this.polygonItem[index].style = `width: ${item.width}px;height: ${item.height}px`
-    })
-  }
-
+  /**
+   * 初始化picture
+   */
   initPicture () {
+    this.initPolygonWrap()
+    this.picture.setAttribute('viewBox', `0 0 ${config.viewBoxWidth} ${config.viewBoxHeight}`)
     this.picturePolygon[0].style = 'stroke:#000000;fill:none;'
     this.picturePolygon[54].style = 'stroke:#000000;fill:none;'
     this.picturePolygon[59].style = 'stroke:#000000;fill:none;'
@@ -121,7 +109,36 @@ class Game {
   }
 
   /**
-   * 移动活动的polygon
+   * 初始化待拖动polygon
+   */
+  initPolygonWrap () {
+    this.polygonArr.forEach((item, index) => {
+      const animatedPoints = item.polygonDom.animatedPoints
+      let str = ''
+      for (let i = 0; i < animatedPoints.length; i++) {
+        const elem = animatedPoints[i]
+        // 1.将碎片重置到0,0
+        // 2.加上屏幕高度 - 自身高度 - 底边距 = y
+        // 3.加上屏幕左边距 + n倍的140 = x
+        str += `${elem.x - item.x + (index * 140) + config.paddingLeft},${elem.y - item.y + config.viewBoxHeight - item.height - config.paddingBottom} `
+      }
+      let polygon = document.createElementNS('http://www.w3.org/2000/svg', 'polygon')
+      let text = document.createElementNS('http://www.w3.org/2000/svg', 'text')
+
+      polygon.setAttribute('style', `fill:${item.color};`)
+      polygon.setAttribute('points', str)
+      text.setAttribute('x', `${item.center.x - item.x - 5 + (index * 140) + config.paddingLeft}`)
+      text.setAttribute('y', `${item.center.y - item.y + 3 + config.viewBoxHeight - item.height - config.paddingBottom}`)
+      text.innerHTML = index + 1
+
+      this.picture.appendChild(polygon)
+      this.picture.appendChild(text)
+
+    })
+  }
+
+  /**
+   * 移动被拖动的polygon
    * @param {number} offsetX 水平方向偏移量
    * @param {number} offsetY 竖直方向偏移量
    */
